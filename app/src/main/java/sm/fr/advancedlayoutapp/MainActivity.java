d0ea773452c321b2ec1d7f106cac466f873e295b
formation <formateur@mail.com>
2018-01-15 16:02:10 +0100

    Ajout dynamique de fragments

--- app/src/main/java/sm/fr/advancedlayoutapp/MainActivity.java
+++ app/src/main/java/sm/fr/advancedlayoutapp/MainActivity.java
@@ -1,7 +1,11 @@
 package sm.fr.advancedlayoutapp;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Ajout d'un fragment à l'écran
+     * @param view
+     */
+    public void onAddFragment(View view) {
+        //Instanciation du fragment
+        FragmentB fragmentB = new FragmentB();
+        //Récupération d'une instance du gestionnaire de fragment
+        FragmentManager manager = getFragmentManager();
+        //Début de la transaction
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.fragmentContainer, fragmentB);
+
+        transaction.commit();
+    }
+
+    public void onReplaceFragment(View view) {
     }
 }
